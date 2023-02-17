5eab75705287d345b3cbd0a3d416d0de06a680ad
RM92023 <robinjara20@gmail.com>
2023-02-16 20:49:35 -0800

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ long int long_number;
 long long int long_long_number;
 char number_char;
 float number_float;	
-printf("Size of a char: %zu byte(s)\n", sizeof(number));
-printf("Size of an int: %zu byte(s)\n", sizeof(long_number));
-printf("Size of a long int: %zu byte(s)\n", sizeof(long_long_number));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(number_char));
-printf("Size of a float: %zu byte(s)\n", sizeof(number_float));
+printf("Size of a char: %lu byte(s)\n", sizeof(number));
+printf("Size of an int: %lu byte(s)\n", sizeof(long_number));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long_long_number));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(number_char));
+printf("Size of a float: %lu byte(s)\n", sizeof(number_float));
 return (0);
 }	
