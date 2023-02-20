b6d65d92e16b8aeec402b3d76b0971d58b95066d
RM92023 <robinjara20@gmail.com>
2023-02-20 13:55:31 -0800

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - print numbers only 0-9 whitout use chart
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+int log;
+for (log = 0; log < 10; log++)
+{
+putchar(log)
+}
+putchar('\n');
+return (0);
+}
