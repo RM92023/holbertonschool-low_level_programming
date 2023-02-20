5b9f4834484d069031a24bd45bfd7e23ec6f74e5
RM92023 <robinjara20@gmail.com>
2023-02-20 13:36:30 -0800

    Adding 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print ten numbers only one digit
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+int log;
+for (log = 0; log <= 9; log++)
+{
+printf("%d", log'\n');
+}
+return (0);
+}
