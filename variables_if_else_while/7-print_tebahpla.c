bdaa3c9ff8d446a457df818ccc911c4e7d36d598
RM92023 <robinjara20@gmail.com>
2023-02-20 14:21:36 -0800

    7-print_tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Reverse Alphabet
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+char tebahpla;
+for (tebahpla = 'z'; tebahpla <= 'a'; tebahpla--)
+{
+putchar(tebahpla);
+}
+putchar('\n');
+return (0);
+}
