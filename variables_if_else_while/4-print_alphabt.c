eb9b15b43e139526005ab6ce7aad2f7fdc48c2d2
RM92023 <robinjara20@gmail.com>
2023-02-20 13:17:30 -0800

    Fix start 4-print_alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,11 @@ int main(void)
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-if(alphabet !='e' && alphabet !='q')
+if (alphabet != 'e' && alphabet != 'q')
 {
 putchar(alphabet);
 }
+putchar('\n');
 }
 return (0);
 }
