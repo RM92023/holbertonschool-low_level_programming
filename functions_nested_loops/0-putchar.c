cc93d3701b9669e35fca655641ca181d7cb49268
RM92023 <robinjara20@gmail.com>
2023-02-21 09:15:49 -0800

    Finally OK 0-putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - function _putchar
- *
- * Return: Always 0.
- *
- */
-
-int main () 
+* main - function _putchar
+*
+* Return: Always 0.
+*
+*/
+int main(void)
 {
 putchar('_');
 putchar('p');
@@ -18,5 +17,5 @@ putchar('h');
 putchar('a');
 putchar('r');
 putchar('\n');
-return(0);
+return (0);
 }
