0c2ee38cc45e8be15a8df66179fb0d77d2e3082c
RM92023 <robinjara20@gmail.com>
2023-02-22 07:47:23 -0800

    Adding 1-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add between a number with other number
+ * @number1: first number
+ * @number2: second number
+ * Return: add between two numbers
+ *
+ */
+int add(int number1, int number2)
+{
+return (number1 + number2);
+}
