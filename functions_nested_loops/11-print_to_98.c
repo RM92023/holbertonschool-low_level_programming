8a1e122657ea74e4a9acd0b2735270791697fcf3
RM92023 <robinjara20@gmail.com>
2023-02-22 08:32:00 -0800

    Adding 11-print_to_98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print all natural numbers until ninetyeight
+ * @n: save natural numbers
+ * Return: ninetyeight natural numbers
+ *
+ */
+void print_to_98(int n)
+{
+printf("\n");
+while (n <= 98)
+{
+printf("%d, ", n);
+n++;
+}
+}
