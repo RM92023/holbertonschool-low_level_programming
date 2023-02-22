908765c5084aa7088058b4b680e974d23e11c603
RM92023 <robinjara20@gmail.com>
2023-02-22 09:07:26 -0800

    fix start 11-print_to_98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,24 @@
  */
 void print_to_98(int n)
 {
-printf("\n");
 while (n <= 98)
 {
-printf("%d, ", n);
-n++;
+printf("%d, ", n++);
 }
 }
+
+
+/*
+ if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	} */
