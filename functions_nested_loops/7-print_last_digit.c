60456444c1c20c18ad65e9dffcd275e018e79b56
RM92023 <robinjara20@gmail.com>
2023-02-21 21:08:30 -0800

    Adding 7-last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int print_last_digit(int n)
+{
+int lastDigit = n % 10;
+if (lastDigit < 0)
+lastDigit *= -1;
+_putchar(lastDigit + '0');
+return (lastDigit);
+}
