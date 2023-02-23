c69e98cc835dd2598d716ccd1e82af27f1d0146d
RM92023 <robinjara20@gmail.com>
2023-02-23 06:59:53 -0800

    adding more functions

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - line until n
+ * @n: parameter
+ * Return : _______
+ *
+ */
+void print_line(int n)
+{
+while (n != 0)
+{
+_putchar('_');
+n++;
+}
+_putchar('\n');
+}
