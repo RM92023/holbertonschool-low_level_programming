1702a17f465adb1a80d9035b36c59466bcff7d2e
RM92023 <robinjara20@gmail.com>
2023-02-22 19:37:43 -0800

    Adding 1-isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - number betwee zero and nine
+ * @c: parameter
+ * Return: number between zero and nine
+ *
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
