e63e818128c1db085b9665cb9637ec0bfa3eb2e5
RM92023 <robinjara20@gmail.com>
2023-02-23 13:31:33 -0800

    Fix Fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,13 @@ int main(void)
 int count;
 for (count = 1; count <= 100; ++count)
 {
-if (count % 3 == 0)
-printf("Fizz");
-if (count % 5 == 0)
+if ((count % 3) == 0 && (count % 5) == 0)
+printf("FizzBuzz");
+else if (count % 5 == 0)
 printf("Buzz");
-if ((count % 3 != 0) && (count % 5 != 0))
+else if ((count % 3) == 0)
+printf("Fizz");
+else
 printf("%d", count);
 printf(" ");
 }
