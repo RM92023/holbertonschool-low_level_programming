d9ba273147cd8efd3493122b09bb34ada6a75c1b
RM92023 <robinjara20@gmail.com>
2023-02-24 20:36:45 -0800

    fibonacci

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
-
+/**
+ * main - print fibbonaci
+ *
+ * Return: fibonacci until 50
+ *
+ */
+int main(void)
+{
+int count;
+long fibonacci1 = 0, fibonacci2 = 1, sumatory;
+for (count = 0; count < 50; count++)
+{
+sumatory = fibonacci1 + fibonacci2;
+printf("%lu", sumatory);
+fibonacci1 = fibonacci2;
+fibonacci2 = sumatory;
+if (count == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
