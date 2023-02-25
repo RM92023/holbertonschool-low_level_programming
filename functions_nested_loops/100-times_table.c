1dcdc0e0d62119ba849db8034dbcfb728ad7a380
RM92023 <robinjara20@gmail.com>
2023-02-24 18:34:12 -0800

    fix start 100-print_times_tables

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * times_table - prints nine times table, starting in zero
- *
+ * @n: parameter
  * Return: nine Times table of multiply
  *
  */
@@ -16,20 +16,19 @@ for (number = 0; number <= n; number++)
 _putchar('0');
 for (multiply = 1; multiply <= n; multiply++)
 {
-	_putchar(',');
-	_putchar(' ');
-	result = number * multiply;
-	
-	if (result >= 100)
-        {
-                _putchar((result / 100) + '0');
-                _putchar((result % 100) / 10 + '0');
-        }
-	else if (result >= 10 && result <= 99)
-	{
-		_putchar((result / 10) + '0');
-	}
-		_putchar((result % 10) + '0');
+_putchar(',');
+_putchar(' ');
+result = number * multiply;
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar((result % 100) / 10 + '0');
+}
+else if (result >= 10 && result <= 99)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
 }
 _putchar('\n');
 }
