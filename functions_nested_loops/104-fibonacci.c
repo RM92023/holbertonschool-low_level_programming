8bed74a54abef35b289a41d38003b56b66870d93
RM92023 <robinjara20@gmail.com>
2023-02-24 20:57:37 -0800

    fix finally

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,8 @@ int main(void)
 {
 int count;
 unsigned long fibonacci1 = 0, fibonacci2 = 1, sumatory;
-unsigned long fibonacci1_half1, fibonacci1_half2, fibonacci2_half1, fibonacci2_half2;
+unsigned long fibonacci1_half1, fibonacci1_half2,
+	      fibonacci2_half1, fibonacci2_half2;
 unsigned long half1, half2;
 for (count = 0; count < 92; count++)
 {
