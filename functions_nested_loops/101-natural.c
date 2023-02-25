98084179ef97adb08de40f45f7b0b7fa90e66228
RM92023 <robinjara20@gmail.com>
2023-02-24 19:21:32 -0800

    fix start 101

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-int increment, sumatory;
+int increment, sumatory = 0;
 for (increment = 0; increment < 1024; increment++)
 {
 if ((increment % 3) == 0 || (increment % 5) == 0)
-sumatory = sumatory +1;
+sumatory = sumatory + 1;
 }
 printf("%d\n", sumatory);
 return (0);
