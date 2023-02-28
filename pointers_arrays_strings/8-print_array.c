abff2b1f1949d55199037db172714172a8cdb6af
RM92023 <robinjara20@gmail.com>
2023-02-28 12:26:28 -0800

    add 8-print_array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - print n array of integers
+ * @a: parameter in pointers
+ * @n: parameter in integer
+ * Return: print arrays
+ *
+ */
+
+void print_array(int *a, int n)
+{
+int count;
+for (count = 0 ; count < n ; count++)
+{
+printf("%d", *(a + count));
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
