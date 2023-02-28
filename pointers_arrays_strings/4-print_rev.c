369a8226c4d934f3bcbbd17f7e1c5f75eaa48fcb
RM92023 <robinjara20@gmail.com>
2023-02-28 08:09:53 -0800

    finally

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,20 @@
 void print_rev(char *s)
 {
 int count;
-while (*(s + count) != '\0')
+/*while (*(s + count) != '\0')
 {
 count++;
 }
 while (count--)
 {
 _putchar(*(s + (count)));
+}
+_putchar('\n');*/
+for (count = 0; s[count] != '\0'; count++)
+;
+for (count = count - 1; s[count] != '\0'; count--)
+{
+	_putchar(s[count]);
 }
 _putchar('\n');
 }
