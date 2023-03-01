950613f96c10b0eab6a377b440921304f9205d63
RM92023 <robinjara20@gmail.com>
2023-03-01 07:27:51 -0800

    Finally

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,10 @@
 char *_strcpy(char *dest, char *src)
 {
 int initial = 0;
-while (src[initial])
+for (initial = 0; src[initial] != '\0'; initial++)
 {
 dest[initial] = src[initial];
-initial++;
 }
+dest[initial++] = '\0';
 return (dest);
 }
