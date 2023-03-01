2d2db1b17ab9dab9cb53e55535382f894dbe625f
RM92023 <robinjara20@gmail.com>
2023-02-28 20:05:22 -0800

    add finally all

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copy strings
+ * @dest: first value
+ * @src: value final
+ * Return: strings
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int initial = 0;
+while (*src != '\0')
+{
+dest[initial] = *src;
+initial++;
+src++;
+}
+return (dest);
+}
