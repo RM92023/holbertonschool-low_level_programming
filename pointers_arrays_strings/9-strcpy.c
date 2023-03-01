6abee8d87f334796cb0214c4f33fe2c50e8c48df
RM92023 <robinjara20@gmail.com>
2023-03-01 11:43:40 -0800

    rename pointer for confirm if is really function

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,7 +12,7 @@ char *_strcpy(char *dest, char *src)
 int initial = 0;
 for (initial = 0; src[initial] != '\0'; initial++)
 {
-dest[initial] = src[initial];
+dest[initial] = *(src + initial);
 }
 dest[initial++] = '\0';
 return (dest);
