b228b410c36cb2f060aa3259dffb9d011d64c97a
RM92023 <robinjara20@gmail.com>
2023-03-02 07:53:00 -0800

    Add 1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncat - concat two strings
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: strings
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, len = 0;
+while (dest[i++])
+	len++;
+for (i = 0; src[i] && i < n; i++)
+	dest[len] = src[i];
+return (dest);
+}
