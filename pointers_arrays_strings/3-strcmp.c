ba26fc4ae80d0f4a4fb00a2a22512edfe0db8491
RM92023 <robinjara20@gmail.com>
2023-03-02 10:09:21 -0800

    fix start finally

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,12 +3,12 @@
  * _strcmp - compare two strings
  * @s1: parameter
  * @s2: parameter
- * Return : compare two strings
+ * Return: compare two strings
  *
  */
 int _strcmp(char *s1, char *s2)
 {
-for(; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
+for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
 ;
 if (*s1 == *s2)
 {
