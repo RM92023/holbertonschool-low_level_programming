58d88c35cf9cbb03a86913b7c082515566d86864
RM92023 <robinjara20@gmail.com>
2023-03-02 09:59:29 -0800

    fix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /**
  * _strcmp - compare two strings
  * @s1: parameter
@@ -9,7 +10,6 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int result;
-result = strcmp(s1, s2);
-return (result);
+char r = strcmp(s1, s2);
+return (r);
 }
