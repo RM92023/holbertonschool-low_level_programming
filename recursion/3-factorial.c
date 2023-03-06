343627529ad2f1e6eef216e07e81ef2a51013e66
RM92023 <robinjara20@gmail.com>
2023-03-06 10:49:07 -0800

    fix start 3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * factorial - factorial is multiply between numbers
+ * @n: parameter
+ * Return: 0
+ *
+ */
 int factorial(int n)
 {
-if (n <= 1)
-return 1;
+if (n >= 0 && n <= 1)
+return (1);
+else if (n < 0)
+return (-1);
 
-return n * factorial (n - 1);
+return (n * factorial(n - 1));
 }
