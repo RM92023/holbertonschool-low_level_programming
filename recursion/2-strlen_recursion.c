9754638bf47b34bb92b1ac07d637289eab818041
RM92023 <robinjara20@gmail.com>
2023-03-06 10:18:49 -0800

    Fix start 2-stlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _stlen_recursion - return length of a string
+ * _strlen_recursion - return length of a string
  * @s: parameter
  * Return: 0
  *
  */
 int _strlen_recursion(char *s)
 {
-	int n;
-	n = strlen(s);
-
-	return (n);
+int n;
+n = strlen(s);
+return (n);
 }
