3fdb6eb0c8fa364e02829bf336c5bea63eeef526
RM92023 <robinjara20@gmail.com>
2023-03-06 10:16:18 -0800

    changing works finish

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - print a string
+ * @s: parameter
+ * Return: 0.
+ *
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+}
---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _stlen_recursion - return length of a string
+ * @s: parameter
+ * Return: 0
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+	n = strlen(s);
+
+	return (n);
+}
