3bc0b99e6dced4708251b1434f2f5c750ece221d
RM92023 <robinjara20@gmail.com>
2023-03-07 06:18:02 -0800

    fix finally

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * is_prime_number - print one if is prime number
- * @n: parameter
+ * primeRecursive - function recursive
+ * @p: parameter
+ * @i: parameter
  * Return: 0
  *
  */
@@ -10,19 +11,27 @@
 int primeRecursive(int p, int i)
 {
 if (p <= 2)
-	return (p == 2);
+return (p == 2);
 if (i >= p)
-	return (1);
+return (1);
 if (p % i == 0)
-	return (0);
+return (0);
 
-return primeRecursive(p, ++i);
+return (primeRecursive(p, ++i));
 }
+
+
+/**
+ * is_prime_number - print one if is prime number
+ * @n: parameter
+ * Return: 0
+ *
+ */
 
 int is_prime_number(int n)
 {
 if (n <= 2 || n % 2 == 0)
 	return (n == 2);
 
-return primeRecursive(n, 3);
+return (primeRecursive(n, 3));
 }
