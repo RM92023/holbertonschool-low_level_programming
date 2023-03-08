58e4226e7bec287b28c79ffd6ef71bce4d3c1d86
RM92023 <robinjara20@gmail.com>
2023-03-08 07:12:21 -0800

    add 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - print all arguments receives
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, resultado;
+if (argc != 3)
+{
+printf("Error: El programa debe recibir exactamente dos argumentos.\n");
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+resultado = num1 *num2;
+printf("%d x %d = %d\n", num1, num2, resultado);
+return (0);
+}
