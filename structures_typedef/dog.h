ce97af988757abc46990e8cf99f40377147b180f
RM92023 <robinjara20@gmail.com>
2023-03-20 13:18:03 -0700

    Fix start

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,22 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - struct initialize
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ *
+ * Return: 0
+ */
+
+
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-}dog_t;
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
