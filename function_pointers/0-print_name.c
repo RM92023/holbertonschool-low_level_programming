4fda990db9b1171b87981abc1752b3488f6e2974
RM92023 <robinjara20@gmail.com>
2023-03-22 07:32:44 -0700

    Actually working in Betyy

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
+ * print_name - function to print name
+ * @name: parameter
+ * @f: parameter
  *
- *
- *
- *
- *
+ * Return 0
  */
 void print_name(char *name, void (*f)(char *))
 {
