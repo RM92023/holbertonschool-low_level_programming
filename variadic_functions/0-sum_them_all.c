5052653f4593dcc24c0379cdbe78b0a6f0ca6372
RM92023 <robinjara20@gmail.com>
2023-03-23 10:18:48 -0700

    Add variadic functions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - function returns sum of all it parameters
+ * @n: parameter
+ *
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+int sum = 0;
+unsigned int i;
+va_list args;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
+va_end(args);
+return (sum);
+}
+
