116b6b6a59d124b7f7fdc556f252f71a7cc395a6
RM92023 <robinjara20@gmail.com>
2023-03-23 11:22:55 -0700

    Add 2-print_string with variadics functions

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - function print string
+ * @separator: parameter
+ * @n: parameter
+ *
+ * Return 0
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+char *str;
+va_list args;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+str = va_arg(args, char *);
+if (str == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", str);
+}
+if (separator != NULL && i != n - 1)
+{
+printf("%s", separator);
+}
+}
+va_end(args);
+printf("\n");
+}
