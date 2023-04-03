4110f7527a61cff2dcef10d4795272c1fcfeb572
RM92023 <robinjara20@gmail.com>
2023-04-03 14:24:49 -0500

    adding 1-list_len.c and 0-print_list.c because is borrow

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * print_list - this function prints the list of elements
+ * @h: the header
+ *
+ * Return: 0
+ */
+size_t list_len(const list_t *h)
+{
+size_t i = 0;
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
+}
