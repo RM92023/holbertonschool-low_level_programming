5b9846dc5adb873ab2886521bcae5069ec3a54c5
RM92023 <robinjara20@gmail.com>
2023-04-03 12:33:26 -0500

    Updating file 0-print_list.c because is necesary organizate betty and space in function printf

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,25 +1,27 @@
 #include "lists.h"
 #include <stdlib.h>
-/**
- * 
- * 
- */
- size_t print_list(const list_t *h)
- {
 
-     size_t i = 0;
-     while (h!= NULL)
-     {
-        if (h->str == NULL)
-        {
-            printf("[0](nil)\n");
-        }
-        else
-        {
-            printf("[%u] %s\n", h->len, h->str);
-        }
-        i++;
-        h = h->next;
-     }
-     return i;
- }
+/**
+ * print_list - this function prints the list of elements
+ * @h: the header
+ *
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+i++;
+h = h->next;
+}
+return (i);
+}
