f12c82a1c61d59605423c14af8335face9ffc988
RM92023 <robinjara20@gmail.com>
2023-04-06 11:00:38 -0500

    adding 1-print_binary.c

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary information about the current process
+ * @num: the number of bytes to print
+ * 
+ * Return: the number of bytes printed
+ */
+
+void print_binary(unsigned long int num)
+{
+unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+int flag = 0;
+while (mask)
+{
+if (num & mask)
+{
+_putchar('1');
+flag = 1;
+}
+else if (flag || mask == 1)
+{
+_putchar('0');
+}
+mask = mask >> 1;
+}
+}
