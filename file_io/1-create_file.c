5b6cfdaacd56e26dfb3988fc5f5345b82fab14a8
RM92023 <robinjara20@gmail.com>
2023-04-10 15:26:56 -0500

    Modify 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,9 +13,13 @@ int create_file(const char *filename, char *text_content)
 {
 int fd, length;
 ssize_t nr_bytes;
-if (!filename || !text_content)
+if (!filename)
 {
 return (-1);
+}
+if (!text_content)
+{
+return (1);
 }
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
