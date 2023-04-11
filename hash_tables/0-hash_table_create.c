1122d8c853c14239d1c61fb2b49ff5390e575f67
RM92023 <robinjara20@gmail.com>
2023-04-11 11:01:44 -0500

    Modify 0-has_tables.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,21 +11,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *hash_table;
 unsigned long int i;
 if (size < 1)
-return NULL;
+return (NULL);
 /* Allocate the table */
 hash_table = malloc(sizeof(hash_table_t));
 if (!hash_table)
-return NULL;
+return (NULL);
 /* Allocate pointers to the head nodes */
 hash_table->array = malloc(sizeof(hash_node_t *) * size);
 if (!hash_table->array)
 {
 free(hash_table);
-return NULL;
+return (NULL);
 }
 /* Initialize the table */
 hash_table->size = size;
 for (i = 0; i < size; i++)
 hash_table->array[i] = NULL;
-return hash_table;
+return (hash_table);
 }
