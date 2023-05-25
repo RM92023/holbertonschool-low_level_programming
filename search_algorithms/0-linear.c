421f48d5adaa95ffcd7159fbef2e5b54984e691a
RM92023 <robinjara20@gmail.com>
2023-05-25 10:56:25 -0500

    Modifing message and space by betty-styles

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -20,7 +20,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Comparing %d with %d\n", array[i], value);
+		printf("Value checked array[%d]=[%d]\n", array[i], value);
 		if (array[i] == value)
 		{
 			return (i);
